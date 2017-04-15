5320b72413c11ec44cc261cd052c3ce7068ee25e
MrLock1378 <rezafaghihi1379@gmail.com>
2017-04-15 21:53:31 +0430

    Create channel.lua

---
+++ plugins/channel.lua
@@ -0,0 +1,26 @@
+do
+ function run(msg, matches)
+return [[ 
+
+➖➖➖➖➖➖➖➖➖
+info🔒LockTeam🔒  : 👀
+Channel : 📢 @LockerTeam
+Support Bot : @KING_DIS_LOVE
+🛡lockerｲ£ⓐʍ🛡 Ƒﾑ:
+طراحی ساخت تبچی و ضد لینک پذیرفته میشود با قیمت کم
+پیام بدید:
+
+@King_dis_love
+➖➖➖➖➖➖➖➖➖
+]]
+end
+return {
+patterns = {
+"^[!/#][Cc]annel$",
+"^[کانال](.*)$",
+"^[!/#][Ll]ocker$",
+"^[!/#][Tt]eam$",
+},
+run = run
+}
+end
